e88393c9a323b35021dce873bb94ba4a16483ab4
jonamx13 <lav.jonathanom@gmail.com>
2023-12-27 16:14:19 -0600

    Constructor method

--- src/Doctor.java
+++ src/Doctor.java
@@ -3,6 +3,14 @@ public class Doctor {
     String name;
     String specialty;
 
+    Doctor() {
+        System.out.println("Building the Doctor object");
+    }
+
+    Doctor(String name) {
+        System.out.println("The assigned Doctor's name is: " + name);
+    }
+
     // Behaviours
     public void showName() {
         System.out.println(name);
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Jonathan Meixueiro");
         myDoctor.name = "Geralt de Rivia";
         myDoctor.showName();
     }
