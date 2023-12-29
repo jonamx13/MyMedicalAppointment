f830362b72101c90b4c16d4b7d20b2d4fdcc639e
jonamx13 <lav.jonathanom@gmail.com>
2023-12-29 14:06:25 -0600

    Static methods (Show id for Doctor Class and Show Menu and ShowPatient Menu for Main Class)

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,18 +1,24 @@
 public class Doctor {
-    int ID;
+    static int id = 0; // Autoincrement
     String name;
     String specialty;
 
     Doctor() {
         System.out.println("Building the Doctor object");
+        id++;
     }
 
     Doctor(String name) {
         System.out.println("The assigned Doctor's name is: " + name);
+        id++;
     }
 
     // Behaviours
     public void showName() {
         System.out.println(name);
+    }
+
+    public void showID() {
+        System.out.println("Doctor ID: " + id);
     }
 }
