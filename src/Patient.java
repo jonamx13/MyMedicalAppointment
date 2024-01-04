b996ce1ed32e075edd9e4d4d4916bcfbf73ae563
jonamx13 <lav.jonathanom@gmail.com>
2024-01-04 13:21:04 -0600

    Patient class

---
+++ src/Patient.java
@@ -0,0 +1,15 @@
+public class Patient {
+    String name;
+    String email;
+    String address;
+    String phoneNumber;
+    String birthday;
+    double weight;
+    double height;
+    String blood;
+
+    Patient(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
