7259cff3f0805f39bb6e16d1c999ad2567c86ed5
jonamx13 <lav.jonathanom@gmail.com>
2024-01-26 12:56:42 -0600

    Nested classes

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Doctor {
     // Attributes
     static int id = 0; // Autoincrement
-    String name;
-    String email;
-    String speciality;
+    private String name;
+    private String email;
+    private String speciality;
 
     Doctor() {
         System.out.println("Building the Doctor object");
@@ -25,5 +28,49 @@ public class Doctor {
 
     public void showID() {
         System.out.println("Doctor ID: " + id);
+    }
+
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+    public void addAvailableAppointment(Date date, String time) {
+        availableAppointments.add(new Doctor.AvailableAppointment(date, time));
+    }
+
+    public ArrayList<AvailableAppointment> getAvailableAppointments() {
+        return availableAppointments;
+    }
+
+    public static class AvailableAppointment {
+        private int id;
+        private Date date;
+        private String time;
+
+        public AvailableAppointment(Date date, String time) {
+            this.date = date;
+            this.time = time;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 import static ui.UIMenu.*;
 
 public class Main {
@@ -5,15 +7,38 @@ public class Main {
         // showMenu();
 
         Doctor myDoctor = new Doctor("Jonathan Meixueiro", "Cardiologist");
-        System.out.println(myDoctor.name);
-        System.out.println(myDoctor.speciality);
+        myDoctor.addAvailableAppointment(new Date(), "4pm");
+        myDoctor.addAvailableAppointment(new Date(), "10am");
+        myDoctor.addAvailableAppointment(new Date(), "1pm");
 
+        System.out.println(myDoctor.getAvailableAppointments());
+
+        for (Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()) {
+            System.out.println(aA.getDate() + " " + aA.getTime());
+        }
+
+        /*System.out.println();
+        System.out.println();
         Patient patient = new Patient("Alexandra", "alexandra@mail.com");
+        Patient patient2 = new Patient("Johan", "johan@mail.com");
+
         patient.setWeight(54.6);
         System.out.println(patient.getWeight());
 
         patient.setPhoneNumber("12345678");
         System.out.println(patient.getPhoneNumber());
+
+        //Object memory behaviour
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+        patient2 = patient;
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+
+        patient2.setName("Mandril");
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());*/
 
     }
 
