af44434361c7fe42605774006118cccb8fbc4034
jonamx13 <lav.jonathanom@gmail.com>
2024-03-01 15:26:08 -0600

    Enumerations

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,35 @@ import java.util.Date;
 import static ui.UIMenu.*;
 
 public class Main {
+    public enum Day {
+        MONDAY("Lunes"),
+        TUESDAY("Martes"),
+        WEDNESDAY("Miercoles");
+
+        private String spanish;
+        private Day(String s) {
+            spanish = s;
+        }
+
+        private String getSpanish() {
+            return spanish;
+        }
+
+    }
     public static void main(String[] args) {
         // showMenu();
+
+        System.out.println(Day.MONDAY);
+        System.out.println(Day.MONDAY.getSpanish());
 
         Doctor myDoctor = new Doctor("Jonathan Meixueiro", "Cardiologist");
         myDoctor.addAvailableAppointment(new Date(), "4pm");
         myDoctor.addAvailableAppointment(new Date(), "10am");
         myDoctor.addAvailableAppointment(new Date(), "1pm");
 
+        // This returns the objects lists created after "myDoctor.addAvailableAppointment"
         System.out.println(myDoctor.getAvailableAppointments());
+
 
         for (Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()) {
             System.out.println(aA.getDate() + " " + aA.getTime());
