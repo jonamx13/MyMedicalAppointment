430080af16c9515e93790b18032978c4517ea509
jonamx13 <lav.jonathanom@gmail.com>
2024-03-05 02:19:09 -0600

    Polimorfism (Overriding methods)

--- src/Main.java
+++ src/Main.java
@@ -37,28 +37,11 @@ public class Main {
             System.out.println(aA.getDate() + " " + aA.getTime());
         }
 
-        /*System.out.println();
+        System.out.println();
         System.out.println();
         Patient patient = new Patient("Alexandra", "alexandra@mail.com");
-        Patient patient2 = new Patient("Johan", "johan@mail.com");
 
-        patient.setWeight(54.6);
-        System.out.println(patient.getWeight());
-
-        patient.setPhoneNumber("12345678");
-        System.out.println(patient.getPhoneNumber());
-
-        //Object memory behaviour
-        System.out.println(patient.getName());
-        System.out.println(patient2.getName());
-        patient2 = patient;
-
-        System.out.println(patient.getName());
-        System.out.println(patient2.getName());
-
-        patient2.setName("Mandril");
-        System.out.println(patient.getName());
-        System.out.println(patient2.getName());*/
+        System.out.println(patient);
 
     }
 
--- src/Patient.java
+++ src/Patient.java
@@ -39,4 +39,10 @@ public class Patient extends User{
     public void setBlood(String blood) {
         this.blood = blood;
     }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nAge: " + birthday + "\nWeight: " + getWeight()
+                + "\nHeight: " + getHeight() + "\nBlood: " + blood;
+    }
 }
